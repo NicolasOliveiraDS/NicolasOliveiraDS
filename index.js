6d0fb0ec0a60a69c400978b57efe4e95655dec06
Nicolas Oliveira da Silva <153407106+NicolasOliveiraDS@users.noreply.github.com>
2024-06-27 22:12:23 -0300

    sOs.study

---
+++ index.js
@@ -0,0 +1,28 @@
+window.revelar = ScrollReveal({reset:true})
+
+// topo do site//
+
+revelar.reveal('.text',{
+    duration: 2000,
+    distance: '30px',
+    delay:500,
+    origin: 'left'
+})
+
+revelar.reveal('#vantagens',{
+    duration: 1000,
+    distance: '50px',
+    delay:500
+})
+
+revelar.reveal('#CADASTRO',{
+    duration: 1000,
+    distance: '30px',
+    delay: 400
+})
+
+revelar.reveal('.footer',{
+    duration: 1000,
+    distance: '10px',
+    delay: 400
+}) 
